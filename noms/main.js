09d64764c94650e2770252ae3b686bbc02926a64
willgames-afk <69923277+willgames-afk@users.noreply.github.com>
2020-08-22 17:15:01 -0700

    Various debugs, improved code annotations

--- noms/main.js
+++ noms/main.js
@@ -19,12 +19,12 @@ function click(e) {
 }
 
 window.addEventListener("mousedown", click, false)
-for (i=0;i<10;i++) {
+for (i=0;i<100;i++) {
     things.make(undefined,undefined,things.randomColor())
 }
 //things.make(20, 20, 'rgb(0,0,0)',1,0)
-//things.make(50,20,'rgb(255,0,0)',-1,0)
-//things.make(60,20,'rgb(0,255,0)',0,0)
+//things.make(50,20,'rgb(255,0,0)',0,0)
+//things.make(80,20,'rgb(0,255,0)',-1,0)
 
 window.onload = function(){requestAnimationFrame(things.main)}
 /*for (k=0;k<20;k++) {
--- noms/nom.js
+++ noms/nom.js
@@ -1,15 +1,16 @@
-canvas = document.getElementById('dotcanvas')
-ctx = canvas.getContext("2d")
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
 things = {
     noms: [],
+    configs: {
+
+    },
     make(
         x = things.randomXInCanvas(10),
         y = things.randomYInCanvas(10),
         color = things.randomColor(),
         xVel = Math.random() * 2 - 1,
-        yVel = Math.random() * 2 - 1
+        yVel = Math.random() * 2 - 1,
+        mass = 10,
+        radius = 10
     ) {
         if (typeof x == 'object' && !Array.isArray(x)) {
             //if it is an object and not an array
@@ -19,7 +20,7 @@ things = {
             yVel = x.yVel
             x = x.x
         }
-        things.noms.push({ x: x, y: y, xVel: xVel, yVel: yVel, color: color, mass: 10, radius: 10, colliding: false })
+        things.noms.push({ x: x, y: y, xVel: xVel, yVel: yVel, color: color, mass: mass, radius: radius, colliding: false })
     },
     render() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -32,32 +33,48 @@ things = {
         }
     },
     process() {
-        canvas.width = window.innerWidth     //Makes canvas the same size as window, no matter what window does   
-        canvas.height = window.innerHeight
-        for (i = 0;i<things.noms.length ; i++) {
+
+        for (i = 0; i < things.noms.length; i++) {
             var nom = things.noms[i]
+            //move noms
             nom.x += nom.xVel
             nom.y += nom.yVel
-            if (nom.x < 10 || nom.x > canvas.width - 10) {
+
+            //check wall collision
+            if (nom.x < nom.radius) {
                 nom.xVel = 0 - nom.xVel
+                nom.x = nom.radius
+            } else if (nom.x > canvas.width - nom.radius) {
+                nom.xVel = 0 - nom.xVel
+                nom.x = canvas.width - nom.radius
             }
-            if (nom.y < 10 || nom.y > canvas.height - 10) {
+            if (nom.y < nom.radius) {
                 nom.yVel = 0 - nom.yVel
+                nom.y = nom.radius
+            } else if (nom.y > canvas.height - 10 && !nom.wallcollide) {
+                nom.yVel = 0 - nom.yVel
+                nom.y = canvas.height - nom.radius
             }
+
             if (things.noms.length > 1) {
+
+                //check collision with other noms
                 nom.closest = things.findClosest(i)
-                //console.log(nom.closest)
                 if (nom.closest.distance < 20) {
+
                     if (!nom.colliding) {
+
                         var nom2 = things.noms[nom.closest.index]
+
                         nom.colliding = true
                         nom2.colliding = true
 
-                        //making sure values don't end up sticking together
+                        //calculating new velocities
                         var newxVel1 = (nom.xVel * (nom.mass - nom2.mass) + (2 * nom2.mass * nom2.xVel)) / (nom.mass + nom2.mass);
                         var newyVel1 = (nom.yVel * (nom.mass - nom2.mass) + (2 * nom2.mass * nom2.yVel)) / (nom.mass + nom2.mass);
                         var newxVel2 = (nom2.xVel * (nom2.mass - nom.mass) + (2 * nom.mass * nom.xVel)) / (nom.mass + nom2.mass);
                         var newyVel2 = (nom2.yVel * (nom2.mass - nom.mass) + (2 * nom.mass * nom.yVel)) / (nom.mass + nom2.mass);
+                        //applying new velocities
                         nom.xVel = newxVel1
                         nom.yVel = newyVel1
                         nom2.xVel = newxVel2
@@ -71,48 +88,79 @@ things = {
         }
     },
     findClosest(thingNumber) {
+        //finds distances to all points, then sorts them and returns 
+        //the index of the closest dot, as well as the distance to that dot.
         distances = things.findDistances(thingNumber)
         distances.sort((a, b) => a.distance - b.distance)
-        //console.log(distances)
         return distances[0]
     },
+
     findFurthest(thingNumber) {
+        //same as findClosest but sorts the opposite way
         distances = things.findDistances(thingNumber)
         distances.sort((a, b) => b.distance - a.distance)
         return distances[0]
     },
+
     findDistances(thingNumber) {
-        noms = things.noms
+        //calculates the distances from the a point to all other points
+        noms = things.noms //convinience
         distances = []
         for (i2 = 0; i2 < noms.length; i2++) {
 
             if (i2 === thingNumber) {
-                //don't count yourself!
+                //Don't count yourself!
                 continue;
             }
 
+            //calculate the distance
             yDist = Math.abs(noms[i2].y - things.noms[thingNumber].y)
             xDist = Math.abs(noms[i2].x - things.noms[thingNumber].x)
+            //push it to distances
             distances.push({ index: i2, distance: Math.sqrt(yDist * yDist + xDist * xDist) })
 
         }
         return distances
     },
     rgbify(r, g, b) {
+        //returns an RGB string from 3 rgb values.
         return 'rgb(' + r + ',' + g + ',' + b + ')'
     },
     randomColor() {
+        //returns a random color in the form of an RGB string
         return things.rgbify(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255))
     },
     main() {
+        //mainloop
         things.process()
         things.render()
         requestAnimationFrame(things.main)
     },
     randomXInCanvas(pad) {
+        //returns a random x coordanate inside canvas with padding.
         return (Math.random() * (canvas.width - 2 * pad)) + pad
     },
     randomYInCanvas(pad) {
+        //same as randomXInCanvas but for Y coordanate
         return (Math.random() * (canvas.height - 2 * pad)) + pad
     },
+    resize(){
+        //resize handler
+        if (document.body.scrollWidth > window.innerWidth) {
+            canvas.width = document.body.scrollWidth
+        } else {
+            canvas.width = window.innerWidth
+        }
+        if (document.body.scrollHeight > window.innerHeight) {
+            canvas.height = document.body.scrollHeight
+        } else {
+            canvas.width = window.innerWidth
+        }
+    },
 }
+
+canvas = document.getElementById('dotcanvas')
+ctx = canvas.getContext("2d")
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+window.addEventListener('resize',things.resize)
