a00a18eb259677993d76030e3df79ab485b45e47
willgames-afk <69923277+willgames-afk@users.noreply.github.com>
2020-09-23 20:40:45 -0700

    Fixed localhostdetector.js

--- localhostdetector.js
+++ localhostdetector.js
@@ -1,5 +1,7 @@
 out = document.getElementById('localhost?')
-if (location.hostname == 'localhost') {
+if (location.port) {
     console.log('Locally Hosted!')
     out.innerHTML = 'Locally Hosted on Port ' + location.port + '!'
+} else if (location.hostname == 'willgames.io') {
+    out.innerHTML = 'Online and hosted at willgames.io'
 }
