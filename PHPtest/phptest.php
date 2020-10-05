f7e9d4cdc1ec4986b2d48e954079b4ccfead87f9
willgames-afk <69923277+willgames-afk@users.noreply.github.com>
2020-10-04 20:17:37 -0700

    Messing about with php

--- PHPtest/phptest.php
+++ PHPtest/phptest.php
@@ -9,15 +9,34 @@
 
 <body>
     <div class='mainbox'>
-        <h1>HEADING GOES HERE</h1>
+        <h1>LOGIN</h1>
         <div class='menu'>
             <p id='localhost?'>Not locally hosted</p>
             <script src='/localhostdetector.js'></script>
             <a href='/'>Homepage</a>
         </div>
 
-        <form method="post" action="form.php">
+        <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             Username: <input type=text name='username'><br>
+            <?php
+                // define variables and set to empty values
+                $username = "";
+
+                if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                    $username = test_input($_GET["username"]) . "\n";
+                    $users = fopen("users.txt", "a");
+                    fwrite($users,$username);
+                    fclose($users);
+                    echo nl2br($username);
+                }
+
+                function test_input($data) {
+                    $data = trim($data);
+                    $data = stripslashes($data);
+                    $data = htmlspecialchars($data);
+                    return $data;
+                }
+            ?><br>
             <input type='submit'>
         </form>
 
