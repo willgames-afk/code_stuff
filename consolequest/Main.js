6617b43319c4cb25a55eb9b570e5509d6fb95187
willgames-afk <69923277+willgames-afk@users.noreply.github.com>
2021-02-16 21:09:12 -0800

    Big changes internally to consoleQuest

--- consolequest/Main.js
+++ consolequest/Main.js
@@ -17,7 +17,7 @@ var aC = { // aC for all commands
   soth: 'soth',
   load: 'load',
   reset: 'reset',
-  exit: 'e',// Didn't like it when I put exits, so it has to be ust exit. 
+  exit: 'e',// Didn't like it when I put exits, so it has to be just exit. 
   map: 'map',
   downloadit: 'dload',
   loadmap: 'mapload',
@@ -60,15 +60,15 @@ function startAll() { // starts the whole thing, triggered by start or init comm
     initCommands();
     if (!theMap.data) {
       alert("I don't have a map, you need to load one!")
-      text = ';Load a map by selecting a file using the file button over there and then typing loadmap.'
+      displayText('Load a map by selecting a file using the file button over there and then typing loadmap.');
       main()
       return 'start'
     } else {
       createCompletionElements()
       startText = theMap.startText
       displayText(startText + '\n' + helpText + '\n' + theMap.data[player.y][player.x].desc)
-      commandExecute.start = false;
-      main();
+      //commandExecute.start = false;
+      //main();
       return 'start'
     }
   } else {
@@ -79,18 +79,19 @@ function startAll() { // starts the whole thing, triggered by start or init comm
 
 function initCommands() { // Very similar to code used in Google's Text Adventure, just wanted to give credit.
   var win = this; // "this" is window, var win is just the window object
-  this.commandExecute = {}; // whether a command was called or not is stored in this object
+  //this.commandExecute = {}; // whether a command was called or not is stored in this object
   var b = {}, command;       //All the b.num and d.num stuff is magic to me.
   for (command in aC) {
     b.num = command;
-    win.commandExecute[aC[b.num]] = false;
+    //win.commandExecute[aC[b.num]] = false;
     Object.defineProperty(window, b.num, {
-      get: function (d) { 
+      get: function (d) {
         return function () {
-          win.commandExecute[aC[d.num]] = true;
+          //win.commandExecute[aC[d.num]] = true;
+          win.commands[aC[d.num]]()
           return d.num;
         }
-      }(b) 
+      }(b)
     });
     b = { num: b.num } //I think this is to detach b from the properties defined in Obect.defineProperty
   }
@@ -144,6 +145,7 @@ function loadFile() { // Loads a map file. Far too complecated, in my opinion.
     console.log("I couldn't load the map, make sure you've selected it by clicking the Choose File button to the left and that the file you selcted is a text file.")
   }
 }
+
 function initSaveSystem() { // cookies coming soon?
   let savedMap = localStorage.getItem('map');
   let savedPlayer = localStorage.getItem('player');
@@ -193,7 +195,7 @@ function createCompletionElements() {
   }
 }
 
-var i = 0
+var i = 0;
 commands = {
   terminate() {
     if (confirm("Are you sure you want to stop this program? You will lose all unsaved progress!")) {
@@ -369,9 +371,9 @@ commands = {
           aI = finalFile[2];
           createCompletionElements();
           if (theMap.data[0][0].vis)
-          localStorage.clear();
+            localStorage.clear();
           console.clear();
-          displayText('New map loaded. Saves have been cleared.','',false);
+          displayText('New map loaded. Saves have been cleared.', '', false);
           startText = theMap.startText;
           displayText(theMap.startText + '\n' + helpText + '\n' + theMap.data[player.y][player.x].desc);
         };
@@ -387,217 +389,48 @@ commands = {
   rem() {
     displayText('\nYou remember when you visited this area...\n%c' + currentCell.desc, 'font-style: italic;');
   },
-}
-
-
-
-
-function processCommands() {
-  if (commandExecute.terminate) {
-    commandExecute.terminate = false
-    if (confirm("Are you sure you want to stop this program? You will lose all unsaved progress!")) {
-      window.close()
-      displayText("Your browser says I can't shut down. You'll just have to close this tab the old fashioned way.", '', false)
+  _move(direction) {
+    if (typeof theMap.directions[direction] == "string") {
+      direction = theMap.directions[theMap.directions[direction]]
     }
-  }
-  if (commandExecute.help) {
-    acceptedCommands = ';Accepted Commands: '
-    for (command in aC) {
-      if (!(command == 'soth')) {
-        acceptedCommands = acceptedCommands + command + ', '
+    console.log(currentCell.exit)
+    console.log(aC[direction])
+    if (currentCell.exit.includes(aC[direction])) {
+      player.lastMove = aC[direction]
+      player.y += theMap.directions[direction].y
+      player.x += theMap.directions[direction].x
+      if (currentCell.vis) {
+        if (currentCell.vdesc) {
+          displayText('\nYou move ' + direction + " and enter a room you've already visited.\n" + currentCell.vdesc)
+        } else {
+          displayText('\nYou move ' + direction + " and enter a room you've already visited.\n%c" + currentCell.desc, 'font-style: italic;')
+        }
+      } else {
+        displayText('\nYou move ' + direction + ' and enter a new area.\n' + currentCell.desc)
       }
-    }
-    acceptedCommands = acceptedCommands.substr(0, acceptedCommands.length - 2) + '.\n\n'
-    displayText(acceptedCommands + "To use an item just type use, I'll know which one you mean")
-    commandExecute.help = false;
-  }
-  if (commandExecute.save) {
-    if (confirm(saveMessage)) {
-      saveGame()
-      displayText("\nSaved your game. Note that some things can erase this save, you can get a more permanant save file by typing downloadit.")
+      if (currentCell.locked && currentCell.locked.comp) {
+        if (currentCell.locked.compText) {
+          displayText('\nYou move ' + direction + " and enter a room you've completed.\n" + currentCell.locked.compText)
+        } else {
+          displayText("\nYou enter a room you've completed. Type remember (rem for short) to remember what happened here")
+        }
+      }
+      currentCell.vis = true;
     }
     else {
-      displayText("\nNo? Well, you can always save later.")
-    };
-    commandExecute.save = false;
-  }
-  if (commandExecute.load) {
-    if (confirm('Loading a save will overwrite your current progress. Are you sure you want to?')) {
-      loadGame()
+      displayText("You can't go that way!")
     }
-    commandExecute.load = false
-  }
-  for (direction in theMap.directions) {
-    if (commandExecute[direction]) {
-      if (typeof theMap.directions[direction] == "string") {
-        direction = theMap.directions[theMap.directions[direction]]
-      }
-      if (currentCell.exit.includes(aC[direction])) {
-        player.lastMove = aC[direction]
-        player.y += theMap.directions[direction].y
-        player.x += theMap.directions[direction].x
-        if (currentCell.vis) {
-          if (currentCell.vdesc) {
-            displayText('\nYou move ' + direction + " and enter a room you've already visited.\n" + currentCell.vdesc)
-          } else {
-            displayText('\nYou move ' + direction + " and enter a room you've already visited.\n%c" + currentCell.desc, 'font-style: italic;')
-          }
-        } else {
-          displayText('\nYou move ' + direction + ' and enter a new area.\n' + currentCell.desc)
-        }
-        if (currentCell.locked && currentCell.locked.comp) {
-          if (currentCell.locked.compText) {
-            displayText('\nYou move ' + direction + " and enter a room you've completed.\n" + currentCell.locked.compText)
-          } else {
-            displayText("\nYou enter a room you've completed. Type remember (rem for short) to remember what happened here")
-          }
-        }
-        currentCell.vis = true;
-      }
-      else {
-        displayText(";You can't go that way!")
-      }
-      commandExecute[direction] = false;
-    }
-  }
-  if (commandExecute.grab) {
-    commandExecute.grab = false;
-    if (currentCell.loot && !Array.isArray(currentCell.loot)) {
-      currentCell.loot = [currentCell.loot]
-    }
-    if (currentCell.loot && currentCell.loot.length > 0) {
-      for (i = 0; i < currentCell.loot.length; i++) {
-        //console.log(i)
-        loot = currentCell.loot.slice(i, i + 1)
-        player.inventory[loot] = aI[loot] // puts the loot from the room into your inventory
-      }
-      currentCell.loot = []//Makes it so that the cell no longer has loot
-      printInventory()
-    } else {
-      currentCell.loot = [];
-      displayText(message.grab)
-    }
-  }
-  if (commandExecute.inventory) {
-    commandExecute.inventory = false;
-    printInventory()
-  }
-  if (commandExecute.use) {
-    commandExecute.use = false;
-    used = false
-    if (currentCell.locked) {
-      if (!Array.isArray(currentCell.locked.use)) {
-        currentCell.locked.use = [currentCell.locked.use]
-      }
-      for (item in player.inventory) { // checks if you have something useful
-
-        for (i = 0; i < currentCell.locked.use.length; i++) {
-
-          if (currentCell.locked.use[i] == item) {
-            used = true
-            exitOpen = ''
-            //If there is a move property...
-            if (currentCell.locked.move) {
-              exitOpen = '\nAn exit opens up to the '
-              if (Array.isArray(currentCell.locked.move)) { //if the move property is an array, we need to add all the array elements
-                if (currentCell.locked.move.length == 1) { // if it's only one long I can't put an 'and' in so this is a thing
-                  exitOpen = exitOpen + currentCell.locked.move[0] + '!'
-                  //console.log("added one from array")
-                  currentCell.exit.push(currentCell.locked.move[0])
-                } else {
-                  for (i = 0; i < currentCell.locked.move.length; i++) {
-                    currentCell.exit.push(currentCell.locked.move[i]) // add the unlocked exit to the exits list
-                    if (i == currentCell.locked.move.length - 1) {
-                      exitOpen = exitOpen + 'and ' + currentCell.locked.move[i] + '!'
-                    } else {
-                      exitOpen = exitOpen + currentCell.locked.move[i] + ', '
-                    }
-                  }
-                  //console.log("added "+i+" from array")
-                }
-              } else { // if the move property isnt an array run the old code
-                exitOpen = exitOpen + currentCell.locked.move + '!'
-                currentCell.exit.push(currentCell.locked.move)
-                //console.log("added string")
-              }
-            }
-            //If there's loot...
-            if (currentCell.locked.loot) {
-              if (!currentCell.loot) {
-                currentCell.loot = [];
-              }
-              currentCell.loot.push(currentCell.locked.loot)
-            }
-
-            displayText(currentCell.locked.onUnlock + exitOpen)
-
-            if (!player.inventory[item].persistant == true) {
-              delete player.inventory[item] // if the item isn't persistant it is deleted after its use
-            }
-            break
-          }
-
-
-        }
-        if (used) {// have to break out of both loops (this one gave me some real trouble)
-          break
-        }
-      }
-      if (!used) {
-        displayText(message.nothingToUse) // you dont have anuything useful
-      }
-    } else {
-      displayText(message.notUse)
-    }
-  }
-  if (commandExecute.soth) {
-    displayText('Something falls into your backpack but climbs out and grabs onto your head. Type inventory to see what it is.')
-    player.inventory.soth = aI.soth
-    commandExecute.soth = false
-  }
-  if (commandExecute.reset) {
-    if (confirm('Do you REALLY want to reset all of you progress and save data!?')) {
-      localStorage.clear();
-      theMap = JSON.parse(originalMap)
-      player = JSON.parse(originalPlayer)
-    }
-  }
-  if (commandExecute.e) {
-    displayText(currentCell.exit, '', false)
-    commandExecute.e = false
-  }
-  if (commandExecute.map) {
-    textmap = [];
-    for (y = 0; y < theMap.data.length; y++) {
-      textmap[y] = [];
-      for (x = 0; x < theMap.data[y].length; x++) {
-        if (curretCell.exit.indexOf('south') != -1) {
-
-        }
-      }
-    }
-  }
-  if (commandExecute.dload) {
-    commandExecute.dload = false;
-    if (confirm("You sure you want to download all the map, item, AND player data for this map?")) {
-      data = JSON.stringify([JSON.parse(originalMap), JSON.parse(originalPlayer), aI]);//It was stringifying each separate thing.
-      download("data", data);
-    }
-  }
-  if (commandExecute.mapload) {
-    commandExecute.mapload = false
-    loadFile()
-  }
-  if (commandExecute.clear) {
-    commandExecute.clear = false
-    console.clear()
-    displayText(helpText + theMap.data[player.y][player.x].desc)
-  }
-  if (commandExecute.rem) {
-    commandExecute.rem = false
-    displayText('\nYou remember when you visited this area...\n%c' + currentCell.desc, 'font-style: italic;')
   }
 }
+for (direction in theMap.directions) {
+  function makeDir(d) { //Fancy Wrapper Function
+    return function() {
+      this._move(d)
+    }
+  }
+  commands[direction] = makeDir(direction);
+}
+
 //   Function that takes care of displaying text. Pretty important for 
 //   a text based game.
 //.  txtras is short for text extras. 
@@ -613,19 +446,6 @@ function displayText(text = '', txtras = '', displayExits = true) {
 //note: can't fully initialize because it doesn't start until you type start, so all the commands except start have to be
 //initiated after that.
 initSaveSystem()
-console.log(' _                         _                  \n/   _   _   _  _  |  _    / \\      _    _  _|_\n\\_ (_) | | _> (_) | (/_   \\_X |_| (/_  _>   | ') 
+console.log(' _                         _                  \n/   _   _   _  _  |  _    / \\      _    _  _|_\n\\_ (_) | | _> (_) | (/_   \\_X |_| (/_  _>   | ')
 console.log('Developed by Will Kam\n')
 console.log('Type START to continue.')
-var terminate = false;
-var startLoop = false;
-//Main Loop
-window.main = function () {
-  stopMain = window.requestAnimationFrame(main);
-  if (terminate) {
-    window.cancelAnimationFrame(stopMain);
-  }
-  processCommands()
-};
-
-
- // Start the cycle
