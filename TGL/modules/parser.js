fad4f4048f4f113b76a4eb8925e7c90b45ae4ee6
willgames-afk <69923277+willgames-afk@users.noreply.github.com>
2021-04-26 19:41:35 -0700

    save

--- TGL/modules/parser.js
+++ TGL/modules/parser.js
@@ -1,77 +1,57 @@
 export function* lexer(string) {
 	function getType(char) {
-		if (leximes[state] && leximes[state].test(char)) {
-			return state;
+		if (leximes[bufferType] && leximes[bufferType].test(char)) {
+			return bufferType;
 		}
-		for(const [key,value] of Object.entries(leximes).sort((a,b)=>{b[0].localeCompare(a[0])})) {
+		for (const [key, value] of leximes) {
+			//console.log (`Testing against ${value}`)
 			if (value.test(char)) {
+				//console.log(`Found it; ${key}`)
 				return key;
 			}
 		}
 	}
-
-	var i = 0;
-	var leximes = {
-		quote: /'/, dbquote: /"/, backquote: /`/,
-		oc: /{/, cc: /}/,
-		ob: /\[/, cb: /\]/,
-		oPar: /\(/, cPar: /\)/,
-		colon: /:/, semicolon: /;/,
-		dot: /\./, //Will try to match things listed higher first; so it'll match
-		number: /[0-9.]/, //Dot before number which will match before alphnum.
-		alphanumeric: /\w/, //It will also try to match the longest possible string.
-	}
-	var hasValue = [
-		"alphanumeric",
-		"number"
+	var leximes = [
+		{quote: /'/}, {dbquote: /"/}, {backquote: /`/},
+		{oc: /{/}, {cc: /}/},
+		{ob: /\[/}, {cb: /\]/},
+		{oPar: /\(/}, {cPar: /\)/},
+		{colon: /:/}, {semicolon: /;/},
+		{dot: /\./}, //Will try to match things listed higher first; so it'll match 'dot' before 'number' alphanum
+		{equals: /=/},
+		{operator: /[*+\-/<=>]/},
+		{number: /[\d.]/},
+		{alphanumeric: /\w/}, //It will also try to match the longest possible string on that type.
 	]
-	var splitters = /[ 	\n"'`{}()[\]]:;/ //note that this starts with space tab, not just 2 spaces
+
+	var splitters = /[ 	\n"'`{}()[\]:;.=+-/*]/ //note that this starts with space tab, not just 2 spaces
+	//Splitters are characters that split a token.
+
 	var buffer = '';
-	var state = 'normal'; //can be normal or string
-	for (i=0;i<string.length;i++) {
+	var bufferType = ''; 
+	for (var i = 0; i < string.length; i++) {
 		var char = string[i];
 		var type = getType(char);
-		switch (state) {
-			case 'alphanumeric':
-				if (type != 'alphanumeric') {
-					yield {type: 'alphanumeric', value: buffer};
-					buffer = '';
-					if (hasValue.includes(type)) {
-						state = type;
-						buffer += char;
-					} else {
-						yield {type: type};
-						state = 'normal'
-					}
-				} else {
-					buffer += char;
-				}
-				break;
-			case 'number':
-				if (type != 'number') {
-					yield {type: 'number', value: buffer};
-					buffer = '';
-					if (hasValue.includes(type)) {
-						state = type;
-						buffer += char
-					} else {
-						yield {type: type};
-						state = 'normal'
-					}
-				} else {
-					buffer += char;
-				}
-				break;
-			default:
-				if (hasValue.includes(type)) {
-					state = type;
-					buffer += char;
-				} else if (type != undefined) {
-					yield {type: type};
-				}
-				break;
+
+		if (splitters.test(char)) {
+
+			if (buffer.length > 0) {
+
+				yield { type: bufferType, value: buffer };
+				buffer = '';
+
+			}
+			if (type != undefined) {
+				yield { type: type };
+			}
+		} else {
+			if (buffer.length == 0) {
+				bufferType = type;
+			}
+			buffer += char;
 		}
 	}
+	yield { type: 'EOF' };
 }
 
 export function parse(lexer) {
